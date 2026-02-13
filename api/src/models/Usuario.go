8372e7dc62e37486c6e303f644c8a378a452d925
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-13 11:56:26 -0300

    config do banco de dados

---
+++ api/src/models/Usuario.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
