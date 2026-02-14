56912e0d660dd6621882cf10b0d8974576cb5e3e
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-14 09:58:48 -0300

    usuarios/validar -> add verificacao de email

--- api/src/models/Usuario.go
+++ api/src/models/Usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 type Usuario struct {
@@ -35,6 +37,10 @@ func (usuario *Usuario) validar(etapa string) error {
 
 	if usuario.Email == "" {
 		return errors.New("O Email é obrigatório e não pode estar em branco")
+	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O email inserido é inválido")
 	}
 
 	if etapa == "cadastro" && usuario.Senha == "" {
