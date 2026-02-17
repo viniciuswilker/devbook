d5d0d1d21395debbab540034bb0462c626b29b34
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-17 12:10:22 -0300

    feat: tela de cadastro e func

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -8,3 +8,7 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
