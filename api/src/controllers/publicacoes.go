1c74ee3cdc4fe8ae93fb02685049c9a2555e1c88
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-16 23:05:17 -0300

    feat: func de criar publicacao

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,8 +1,54 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositorios"
+	"api/src/response"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		response.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		response.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao models.Publicacao
+
+	publicacao.AutorID = usuarioID
+
+	if erro := json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	response.JSON(w, http.StatusCreated, publicacao)
 
 }
 
