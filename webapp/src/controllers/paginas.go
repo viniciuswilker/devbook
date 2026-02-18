f2c2b1a0dd2c8b54700f1eb9b9f3a1448e72cb85
Vinicius Wilker <vinicius.zoom360@gmail.com>
2026-02-18 12:16:14 -0300

    fix(ajuste) melhora nas pagina pagina de home e header

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/models"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
@@ -41,12 +43,15 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cookie, _ := cookies.Ler(r)
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
 	utils.ExecutarTemplate(w, "home.html", struct {
 		Publicacoes []models.Publicacao
-		OutroCampo  string
+		UsuarioID   uint64
 	}{
 		Publicacoes: publicacoes,
-		OutroCampo:  "Valor Qualquer",
+		UsuarioID:   usuarioID,
 	})
 
 }
